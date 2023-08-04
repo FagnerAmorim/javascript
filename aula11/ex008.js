2b3c04fbe29a84bd2a325c24bcd8d68ab945da77
FagnerAmorim <fuinha.fagner2008@hotmail.com>
2023-08-04 12:04:17 -0300

    Criei aula 11 e exx 08,09,10

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+console.log('O consola funcionou corretamente')
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {//condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') {//condições com  dois blocos if e else que usa os termos true e false
+    console.log('Você é Estrangeiro')
+} else {
+    console.log('Você é Brasileiro')
+}
