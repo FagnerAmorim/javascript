528f601e3cc2328609103876d4cecad48b839ad6
FagnerAmorim <fuinha.fagner2008@hotmail.com>
2023-08-06 12:19:46 -0300

    atualização ex 009

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,6 +1,6 @@
 var pais = 'Brasil'
 console.log(`Vivendo em ${pais}`)
-if (pais != 'Brasil') {//condições com  dois blocos if e else que usa os termos true e false
+if (pais != 'Brasil') {//condições compostas com  dois blocos if e else que usa os termos true e false
     console.log('Você é Estrangeiro')
 } else {
     console.log('Você é Brasileiro')
