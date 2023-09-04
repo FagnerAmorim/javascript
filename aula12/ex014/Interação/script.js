186932f00a3aeaa44ee014f5dc16b1f6cc3735f7
FagnerAmorim <fuinha.fagner2008@hotmail.com>
2023-09-04 18:59:39 -0300

    término do exercicio

--- aula12/ex014/Interação/script.js
+++ aula12/ex014/Interação/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        //BOA NOITE!
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
+    }
+}
