a73ac09651b79fff02ca21b6aaecc3c662b864de
FagnerAmorim <fuinha.fagner2008@hotmail.com>
2023-09-08 19:18:57 -0300

    criei aula 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`Passo${c}`)
+    c++
+} while (c <= 10)
+
+
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,22 @@
+/*var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+*/
+
+/*
+var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+}
+*/
+
+//--------------------
+
+console.log('Vai começar...')
+for(var c = 1;c <= 4;c++) {
+    console.log(c)
+}
+console.log('FIM!')
